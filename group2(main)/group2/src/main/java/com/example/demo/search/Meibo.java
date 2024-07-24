a715dfc80355e63fcb30a7b93df8152c957d96c1
ryos <ryo27.ttyo@gmail.com>
2024-07-24 21:04:50 +0900

    kannkyousyokitouroku

---
+++ group2(main)/group2/src/main/java/com/example/demo/search/Meibo.java
@@ -0,0 +1,108 @@
+package com.example.demo.search;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Meibo {
+    private int id;
+    private String name;
+    private int age;
+    private LocalDate sdate;
+    private LocalDate edate;
+    private String password;
+
+    // Default constructor
+    public Meibo() {
+    }
+
+    // Parameterized constructor
+    public Meibo(int id, String name, int age, LocalDate sdate, LocalDate edate, String password) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sdate = sdate;
+        this.edate = edate;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getSdate() {
+        return sdate;
+    }
+
+    public void setSdate(LocalDate sdate) {
+        this.sdate = sdate;
+    }
+
+    public LocalDate getEdate() {
+        return edate;
+    }
+
+    public void setEdate(LocalDate edate) {
+        this.edate = edate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Meibo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sdate=" + sdate +
+                ", edate=" + edate +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    // equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Meibo meibo = (Meibo) o;
+        return id == meibo.id &&
+                age == meibo.age &&
+                Objects.equals(name, meibo.name) &&
+                Objects.equals(sdate, meibo.sdate) &&
+                Objects.equals(edate, meibo.edate) &&
+                Objects.equals(password, meibo.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, sdate, edate, password);
+    }
+}
