9fda82ad5d2076718ffa2ef3bf2aedd49cad2667
ryos <ryo27.ttyo@gmail.com>
2024-08-10 16:54:27 +0900

    検索画面仮

--- grp2/src/main/java/com/example/demo/MeiboForm.java
+++ grp2/src/main/java/com/example/demo/MeiboForm.java
@@ -4,15 +4,20 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class MeiboForm {
-    
-    private int id;
-    private String name;
-    private int age;
-    private LocalDate sdate;
-    private LocalDate edate;
-    private String password;
 
-    // Getters and Setters
+    private int id; // 社員ID
+    private String name; // 社員の名前
+    private int age; // 年齢
+    private LocalDate sdate; // 開始日
+    private LocalDate edate; // 終了日
+    private String password; // パスワード
+    private Integer ageStart;
+    private Integer ageEnd;
+    private LocalDate sdateStart;
+    private LocalDate sdateEnd;
+    private LocalDate edateStart;
+    private LocalDate edateEnd;
+
     public int getId() {
         return id;
     }
@@ -61,6 +66,80 @@ public class MeiboForm {
         this.password = password;
     }
 
+    public Integer getAgeStart() {
+        return ageStart;
+    }
+
+    public void setAgeStart(Integer ageStart) {
+        this.ageStart = ageStart;
+    }
+
+    public Integer getAgeEnd() {
+        return ageEnd;
+    }
+
+    public void setAgeEnd(Integer ageEnd) {
+        this.ageEnd = ageEnd;
+    }
+
+    public LocalDate getSdateStart() {
+        return sdateStart;
+    }
+
+    public void setSdateStart(LocalDate sdateStart) {
+        this.sdateStart = sdateStart;
+    }
+
+    public LocalDate getSdateEnd() {
+        return sdateEnd;
+    }
+
+    public void setSdateEnd(LocalDate sdateEnd) {
+        this.sdateEnd = sdateEnd;
+    }
+
+    public LocalDate getEdateStart() {
+        return edateStart;
+    }
+
+    public void setEdateStart(LocalDate edateStart) {
+        this.edateStart = edateStart;
+    }
+
+    public LocalDate getEdateEnd() {
+        return edateEnd;
+    }
+
+    public void setEdateEnd(LocalDate edateEnd) {
+        this.edateEnd = edateEnd;
+    }
+
+    // バリデーションメソッド: フォームのフィールドが有効かどうかをチェックする
+    public boolean isValid() {
+        return isNameValid() && isPasswordValid() && isAgeValid() && isDateValid();
+    }
+
+    // 名前のバリデーション: 名前が null でなく、空でなく、長さが 100 文字以下であるかをチェック
+    private boolean isNameValid() {
+        return name != null && !name.isEmpty() && name.length() <= 100;
+    }
+
+    // パスワードのバリデーション: パスワードが null でなく、長さが 8 文字以上 20 文字以下であるかをチェック
+    private boolean isPasswordValid() {
+        return password != null && password.length() >= 8 && password.length() <= 20;
+    }
+
+    // 年齢のバリデーション: 年齢が 0 以上であるかをチェック
+    private boolean isAgeValid() {
+        return age >= 0;
+    }
+
+    // 日付のバリデーション: 開始日が終了日より前であり、両方が null でないことをチェック
+    private boolean isDateValid() {
+        return sdate != null && edate != null && !sdate.isAfter(edate);
+    }
+
+    // オブジェクトの等価性を判断するメソッド: 他のオブジェクトと比較し、すべてのフィールドが一致するかを確認
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -74,20 +153,21 @@ public class MeiboForm {
                 Objects.equals(password, meibo.password);
     }
 
+    // オブジェクトのハッシュコードを生成するメソッド: オブジェクトのハッシュコードを計算し、一意に識別できるようにする
     @Override
     public int hashCode() {
         return Objects.hash(id, name, age, sdate, edate, password);
     }
 
+    // オブジェクトを文字列に変換するメソッド: オブジェクトの内容を文字列として表現する
     @Override
     public String toString() {
-        return "Meibo{" +
+        return "MeiboForm{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 ", sdate=" + sdate +
                 ", edate=" + edate +
-                ", password='" + password + '\'' +
                 '}';
     }
 }
---
+++ grp2/src/main/java/com/example/demo/UpdateController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UpdateController {
+
+    @Autowired
+    private SearchService searchService;
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable("id") Integer id, Model model) {
+        MeiboForm meibo = searchService.findById(id);
+        if (meibo != null) {
+            model.addAttribute("meibo", meibo);
+            return "update"; // update.html に遷移
+        } else {
+            model.addAttribute("errorMessage", "指定された社員情報は存在しません。");
+            return "error"; // エラーページに遷移
+        }
+    }
+}
